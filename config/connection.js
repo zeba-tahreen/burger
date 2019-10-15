6274919937219903a3916da1bc947bd5047220d2
Zeba <50170893+zeba-tahreen@users.noreply.github.com>
2019-10-15 18:42:26 -0400

    update the new changes in connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+  if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-  {
-  port: 3306,
-  host: "bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "mhxphsorfyqdssq4",
-  password: "ze8hzmcyjed3ssxc",
-  database: "vzk3mttlun0wttu3"
-});
-
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
